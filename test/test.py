dffeed8fc9a7fdadc5d60152a310e263f895a44f
tomonori-sakagami <sakagami0615@gmail.com>
2020-07-25 01:34:43 +0900

    fix:  意図したエラーチェックを変更

--- test/test.py
+++ test/test.py
@@ -11,5 +11,6 @@ from src.myadd import addition
 class MathTest(unittest.TestCase):
 	def test_addition(self):
 		actual = addition(3,4)
-		expected = 8 # 実行結果と一致
+		#expected = 8 # 実行結果と一致
+		expected = 7 # 実行結果と一致
 		self.assertEqual(actual, expected)
