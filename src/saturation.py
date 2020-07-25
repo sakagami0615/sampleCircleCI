eed07726bc89762a7e47f94bfd43128384345b94
tomonori-sakagami <sakagami0615@gmail.com>
2020-07-26 01:43:16 +0900

    fix: autopep8を実施

--- src/saturation.py
+++ src/saturation.py
@@ -1,7 +1,7 @@
 def saturation(value, high, low):
-	if (value <= low):
-		return low
-	elif (value < high):
-		return value
-	else:
-		return high
+    if (value <= low):
+        return low
+    elif (value < high):
+        return value
+    else:
+        return high
--- test/test.py
+++ test/test.py
@@ -1,19 +1,18 @@
+from src.saturation import saturation
+import unittest
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-import unittest
-from src.saturation import saturation
-
 
 class MathTest(unittest.TestCase):
-	
-	def test_saturation(self):
-		test_datas = [
-			{'value':1, 'high':10, 'low':5, 'expect': 5},
-			{'value':7, 'high':10, 'low':5, 'expect': 7},
-			{'value':15, 'high':10, 'low':5, 'expect': 10}
-		]
-		for data in test_datas:
-			actual = saturation(data['value'], data['high'], data['low'])
-			self.assertEqual(actual, data['expect'])
+
+    def test_saturation(self):
+        test_datas = [
+            {'value': 1, 'high': 10, 'low': 5, 'expect': 5},
+            {'value': 7, 'high': 10, 'low': 5, 'expect': 7},
+            {'value': 15, 'high': 10, 'low': 5, 'expect': 10}
+        ]
+        for data in test_datas:
+            actual = saturation(data['value'], data['high'], data['low'])
+            self.assertEqual(actual, data['expect'])
